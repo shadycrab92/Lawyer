8dbd4f53aa9beea37db706d2ed876c2c97cc9b68
shadycrab92 <art.belichenko@gmail.com>
2017-12-06 19:00:32 +0300

    bulma

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>React Slingshot</h1>
-
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="/fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
+    <div className="container">
+      <h1 className="title">
+        Hello World
+      </h1>
+      <p className="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
     </div>
   );
 };
